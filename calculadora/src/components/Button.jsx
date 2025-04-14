53fa069ae0d75cae740f9a9041396ca34a76aafe
joao-souza-flooxai <joao.souza.flooxai@gmail.com>
2025-04-14 11:58:36 -0300

    feat: Finshed the layout setup. Now working on the logic part.

--- calculadora/src/components/Button.jsx
+++ calculadora/src/components/Button.jsx
@@ -1,7 +1,18 @@
-
 import React from 'react';
 import './Button.css';
 
 export default function Button(props) {
-   return( <button className="button">{props.label}</button>)
+   return( 
+         <button 
+            onClick={() => props.click && props.click(props.label)}
+            className={`
+               button
+               ${props.operation ? 'operation' : ''} 
+               ${props.double ? 'double' : ''} 
+               ${props.triple ? 'triple' : ''} 
+            `}
+         >
+            {props.label}
+         </button>
+         )
 };
--- calculadora/src/main/Calculator.jsx
+++ calculadora/src/main/Calculator.jsx
@@ -2,28 +2,47 @@ import React, {Component} from "react";
 import "./Calculator.css";
 
 import Button from "../components/Button"
+import Display from "../components/Display"
 
 export default class Calculator extends Component{
+
+
+
+    clearDisplay(){
+
+
+    }
+
+    addDigit(){
+
+    }
+
+    setOperation(){
+
+    }
+
     render(){
+
         return(
             <div className="calculator">
-            <Button label="AC"/>
-            <Button label="/"/>
-            <Button label="7"/>
-            <Button label="8"/>
-            <Button label="9"/>
-            <Button label="*"/>
-            <Button label="4"/>
-            <Button label="5"/>
-            <Button label="6"/>
-            <Button label="-"/>
-            <Button label="1"/>
-            <Button label="2"/>
-            <Button label="3"/>
-            <Button label="+"/>
-            <Button label="0"/>
-            <Button label="."/>
-            <Button label="="/>
+            <Display value="0" />
+                <Button label="AC" click={() => this.clearDisplay()} triple />
+                <Button label="/" click={() => this.setOperation("/")} operation />
+                <Button label="7" click={() => this.addDigit(7)} />
+                <Button label="8" click={() => this.addDigit(8)} />
+                <Button label="9" click={() => this.addDigit(9)} />
+                <Button label="*" click={() => this.setOperation("*")} operation/>
+                <Button label="4" click={() => this.addDigit(4)} />
+                <Button label="5" click={() => this.addDigit(5)} />
+                <Button label="6" click={() => this.addDigit(6)} />
+                <Button label="-" click={() => this.setOperation("-")} operation/>
+                <Button label="1" click={() => this.addDigit(1)} />
+                <Button label="2" click={() => this.addDigit(2)} />
+                <Button label="3" click={() => this.addDigit(3)} />
+                <Button label="+" click={() => this.setOperation("+")} operation/>
+                <Button label="0" click={() => this.addDigit(0)} double/>
+                <Button label="." click={() => this.addDigit(".")} />
+                <Button label="=" click={() => this.setOperation("=")} operation/>
             </div>
         );
     }
